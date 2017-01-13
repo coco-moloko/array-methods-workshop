08aa5b905f277876eedd9e9107e3ccbe14df1854
Coco Moloko <courtneyb.brecht@gmail.com>
2017-01-13 18:49:15 +0000

    answered highLow function question

--- answers.js
+++ answers.js
@@ -37,16 +37,77 @@ var filterArray = Array.prototype.filter;
 function theWholeThing(str) {
     var rayTheArray = str.split(" ");
     var theWord = "";
-    var hi = rayTheArray.reduce(function(a, b) {
+    var red = rayTheArray.reduce(function(a, b) {
         if (a.length < b.length) {
         theWord = b;
         }
     return theWord;
 });
 
-return hi;
+return red;
 }
 var rayTheString = "Hello I am the superhero of the day!";
 
 console.log(theWholeThing(rayTheString));
 
+
+//count vowels
+
+// function countVowels(str) {
+//     var chars = str.split("");
+//     var number = 0;
+//     chars.forEach(function(a){
+//         if (a == "a" || a == "e" || a == "i"|| a == "o"|| a == "u") {
+//             number ++;
+//         }
+        
+//     });
+    
+//     return number;
+// }
+
+// var stringTheString = "The best movie ever is full metal jacket";
+
+// console.log(countVowels(stringTheString));
+
+//change below to reduce
+function countVowels(str) {
+    var chars = str.split("");
+    var number = 0;
+    chars.forEach(function(a){
+        if (a == "a" || a == "e" || a == "i"|| a == "o"|| a == "u") {
+            number ++;
+        }
+        
+    });
+    
+    return number;
+}
+
+var stringTheString = "The best movie ever is full metal jacket";
+
+console.log(countVowels(stringTheString));
+
+//high/low function
+
+var numbers = [1, -10, 20, 40, 5];
+
+function highLow(arr) {
+	return arr.reduce(function(currentExtremes, num){
+		if (num < currentExtremes.lowest) {
+			currentExtremes.lowest = num;
+		}
+
+		if (num > currentExtremes.highest) {
+			currentExtremes.highest = num;
+		}
+
+		return currentExtremes;
+
+	}, {lowest: Infinity, highest: -Infinity});
+
+}
+
+console.log(highLow(numbers));
+
+//wheel of fortune
